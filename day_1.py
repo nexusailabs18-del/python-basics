037ce624d27aa7175530475906dc2a4a2e7fd72e
nexusailabs18-del <nexusailabs18@gmail.com>
2026-04-05 16:50:16 +0530

    Create day_1.py
    ⚡ Variables — storing and managing data
    ⚡ Strings — working with text dynamically
    ⚡ String formatting — using variables inside outputs
    ⚡ Case handling — transforming text (upper/lower)

---
+++ day_1.py
@@ -0,0 +1,27 @@
+first_name = "ama"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}!"
+print(message)
+# answer: Hello, Ama Lovelace!     
+
+hobby = "running and cooking python"
+print(hobby)
+hobby = "cooking maths and physics"
+print(hobby)
+# answer running and cooking python
+# answer cooking maths and physics 
+
+first_name = "ama"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(f"Hello, {full_name.title()}!")
+# answer: Hello, Ama Lovelace!
+
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+# answer Ada Lovelace
+# answer ADA LOVELACE
+# answer ada lovelace
